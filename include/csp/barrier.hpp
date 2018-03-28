60dc80981076897aafa36f19a15865a57d65169f
Kevin Chalmers <k.chalmers@napier.ac.uk>
2018-03-28 11:50:32 +0100

    Added barrier data bundle.

--- include/csp/barrier.hpp
+++ include/csp/barrier.hpp
@@ -1,15 +1,31 @@
 #pragma once
 
+#include <mutex>
+#include <condition_variable>
+
 namespace csp
 {
 	class barrier
 	{
+	private:
+		struct barrier_data
+		{
+			size_t enrolled = 0;
+			size_t count_down = 0;
+			std::mutex mut;
+			std::condition_variable cond;
+		};
+
+		std::shared_ptr<barrier_data> _internal = nullptr;
+
 	public:
 		barrier()
+			: _internal(std::make_shared<barrier_data>())
 		{
 		}
 
 		barrier(size_t size)
+			: barrier(), _internal->enrolled(size), _internal->count_down(size)
 		{
 		}
 
