057e51b3acc41b1cfc9ceb86f2e95c3c7cb9afa6
Kevin Chalmers <k.chalmers@napier.ac.uk>
2018-03-28 12:18:07 +0100

    Added par data type.

--- include/csp/parallel.hpp
+++ include/csp/parallel.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <memory>
 #include <functional>
 #include <thread>
+#include <vector>
+#include <set>
 #include "barrier.hpp"
 #include "process.hpp"
 
@@ -12,7 +15,7 @@ namespace csp
 	private:
 		std::function<void()> _process;
 		std::shared_ptr<std::thread> _thread = nullptr;
-		barrier _terminate;
+		barrier _bar;
 		barrier _park = barrier(2);
 		bool _running = true;
 	public:
@@ -21,38 +24,49 @@ namespace csp
 		}
 
 		par_thread(std::function<void()> &proc, barrier &bar)
+			: _process(proc), _bar(bar)
 		{
 		}
 
-		~par_thread()
-		{
-
-		}
+		~par_thread();
 
 		void reset(std::function<void()> proc, barrier &bar) noexcept
 		{
+			_process = proc;
+			_bar = bar;
+			_running = true;
 		}
 
 		void terminate() noexcept
 		{
+			_running = false;
+			_park.sync();
 		}
 
 		void release() noexcept
 		{
+			_park.sync();
 		}
 
-		void run() noexcept
-		{
-		}
+		void run() noexcept;
 
-		void start() noexcept
-		{
-		}
+		void start() noexcept;
 	};
 
 	class par : public process
 	{
+		friend class par_thread;
 	private:
+
+		struct par_data
+		{
+			std::mutex mut;
+			std::vector<std::function<void()>> processes;
+			std::vector<std::shared_ptr<par_thread>> threads;
+			barrier bar = barrier(0);
+			bool processes_changed = true;
+		};
+
 		void release_all_threads() noexcept
 		{
 		}
@@ -89,4 +103,26 @@ namespace csp
 
 		}
 	};
+
+	par_thread::~par_thread()
+	{
+		par::remove_from_all_threads(_thread);
+	}
+
+	void par_thread::run() noexcept
+	{
+		while (_running)
+		{
+			_process();
+			_bar.sync();
+			_park.sync();
+		}
+	}
+
+	void par_thread::start() noexcept
+	{
+		_thread = std::make_shared<std::thread>(&par_thread::run, this);
+		par::add_to_all_threads(_thread);
+		_running = true;
+	}
 }
