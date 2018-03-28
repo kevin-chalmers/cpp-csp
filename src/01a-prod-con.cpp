2475b9472c6fee60bdd0e826bab1a20a7813234a
Kevin Chalmers <k.chalmers@napier.ac.uk>
2018-03-28 10:30:26 +0100

    Updated prod-con to use threads.

--- src/01a-prod-con.cpp
+++ src/01a-prod-con.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <thread>
 
 using namespace std;
 
+void producer()
+{
+	cout << "Producer started" << endl;
+}
+
+void consumer()
+{
+	cout << "Consumer started" << endl;
+}
+
 int main(int argc, char **argv) noexcept
 {
-	cout << "Hello world" << endl;
+	thread prod(producer);
+	thread con(consumer);
+	prod.join();
+	con.join();
 	return 0;
 }
