db55d1e6ad933b0c89914a514c5e88eec27f27c2
Kevin Chalmers <k.chalmers@napier.ac.uk>
2018-03-28 10:40:57 +0100

    Added basic channel interface.

--- include/csp/channel.hpp
+++ include/csp/channel.hpp
@@ -5,6 +5,50 @@ namespace csp
 	template<typename T>
 	class channel
 	{
+	public:
+		void write(T value)
+		{
 
+		}
+
+		T read()
+		{
+			return T();
+		}
+
+		T start_read()
+		{
+			return T();
+		}
+
+		void end_read()
+		{
+
+		}
+
+		bool enable() noexcept
+		{
+			return false;
+		}
+
+		bool disable() noexcept
+		{
+			return false;
+		}
+
+		bool pending() const noexcept
+		{
+			return false;
+		}
+
+		void reader_poison(size_t strength) noexcept
+		{
+
+		}
+
+		void writer_poison(size_t strength) noexcept
+		{
+
+		}
 	};
 }
