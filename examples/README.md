7eb7c036f78e7cc88038a6ac9a4686e5bc1e8b7b
Kevin Chalmers <k.chalmers@napier.ac.uk>
2018-08-09 12:02:46 +0100

    Updated producer-consumer to be more generic.

---
+++ examples/README.md
@@ -0,0 +1,5 @@
+# Example Programs
+
+[Producer-Consumer](producer-consumer) A basic example where a single process (the producer) sends to 
+another process (the consumer).  The application requests input from the user in the producer process.  The
+producer will send the data to the consumer where the input is printed to the console.
---
+++ examples/producer-consumer/prod-con.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <csp/csp.hpp>
+
+using namespace std;
+using namespace csp;
+
+class producer : public process
+{
+    chan_out<string> _out;
+
+public:
+    producer(chan_out<string> out)
+    : _out(move(out))
+    {
+    }
+
+    void run() noexcept final
+    {
+        string str;
+        while (str != "exit")
+        {
+            cout << "Enter data to send: ";
+            cin >> str;
+            _out(str);
+        }
+    }
+};
+
+class consumer : public process
+{
+    chan_in<string> _in;
+
+public:
+    consumer(chan_in<string> in)
+    : _in(move(in))
+    {
+    }
+
+    void run() noexcept final
+    {
+        string str;
+        while (str != "exit")
+        {
+            str = _in();
+            cout << str << endl;
+        }
+    }
+};
+
+int main(int argc, char **argv) noexcept
+{
+    // Select the concurrency model to use
+    using model = thread_model;
+	auto c = model::make_one2one<string>();
+
+	parallel<thread_model>
+    {
+        make_proc<producer>(c),
+        make_proc<consumer>(c)
+    }();
+
+	return 0;
+}
