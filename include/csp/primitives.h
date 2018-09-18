a6535a8e3b535c1f882111fd544bc6f2431af1a5
Kevin Chalmers <k.chalmers@napier.ac.uk>
2018-09-18 13:54:30 +0100

    Added skip and stop primitivies.

--- include/csp/primitives.h
+++ include/csp/primitives.h
@@ -1,6 +1,67 @@
 #pragma once
 
+#include "process.hpp"
+#include "alternative.hpp"
+
 namespace csp
 {
+    class skip : public process, public guard
+    {
+    private:
+        class skip_internal : public guard_internal
+        {
+            bool enable(alt_internal*) { return true; }
+            bool disable() { return true; }
+        };
+    public:
+        skip()
+        : _internal(std::make_shared<skip_internal>())
+        {
+        }
 
+        skip(const skip&) = default;
+
+        skip(skip&&) = default;
+
+        ~skip() = default;
+
+        skip& operator=(const skip&) = default;
+
+        skip& operator=(skip&&) = default;
+
+        void run() final
+        {
+        }
+    };
+
+    class stop : public process, public guard
+    {
+    private:
+        class stop_internal : public guard_internal
+        {
+            bool enable(alt_internal*) { return false; }
+            bool disable() { return false; }
+        };
+    public:
+        stop()
+        : _internal(std::make_shared<skip_internal>())
+        {
+        }
+
+        stop(const stop&) = default;
+
+        stop(stop&&) = default;
+
+        ~stop() = default;
+
+        stop& operator=(const stop&) = default;
+
+        stop& operator=(stop&&) = default;
+
+        void run() final
+        {
+            auto c = make_one2one<int>();
+            c();
+        }
+    };
 }
--- include/csp/timer.h
+++ include/csp/timer.h
@@ -6,5 +6,5 @@
 
 namespace csp
 {
-    
+
 }
