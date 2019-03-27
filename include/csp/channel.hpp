3699d324d5c878d0bbdeb681bb2df9f60f453d3a
Kevin Chalmers <k.chalmers@napier.ac.uk>
2019-03-27 21:07:41 +0000

    Added channel end tests.

--- include/csp/channel.hpp
+++ include/csp/channel.hpp
@@ -8,28 +8,52 @@
 namespace csp
 {
     template<typename T>
-    class one2one_chan
+    class chan_in
     {
-
     };
 
     template<typename T>
-    class one2any_chan
+    class guarded_chan_in : public chan_in<T>
     {
-
     };
 
     template<typename T>
-    class any2one_chan
+    class shared_chan_in : public chan_in<T>
     {
-
     };
 
     template<typename T>
-    class any2any_chan
+    class chan_out
     {
-
     };
+
+    template<typename T>
+    class shared_chan_out : public chan_out<T>
+    {
+    };
+
+    template<typename T, template<typename> typename INPUT_END, template<typename> typename OUTPUT_END>
+    class chan_type
+    {
+    private:
+        INPUT_END<T> _input;
+        OUTPUT_END<T> _output;
+    public:
+        inline INPUT_END<T> in() const { return _input; }
+        inline OUTPUT_END<T> out() const { return _output; }
+    };
+
+    template<typename T>
+    using one2one_chan = chan_type<T, guarded_chan_in, chan_out>;
+
+    template<typename T>
+    using one2any_chan = chan_type<T, shared_chan_in, chan_out>;
+
+    template<typename T>
+    using any2one_chan = chan_type<T, guarded_chan_in, shared_chan_out>;
+
+    template<typename T>
+    using any2any_chan = chan_type<T, shared_chan_in, shared_chan_out>;
 }
 
 #endif //CPP_CSP_CHANNEL_H
