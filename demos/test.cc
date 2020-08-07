a4bd30c05085fe010eca6ec1b3a5da02b65a1326
Kevin Chalmers <kevin.chalmers@roehampton.ac.uk>
2020-08-07 15:33:16 +0100

    Started work on tests.

---
+++ demos/test.cc
@@ -0,0 +1,13 @@
+//
+// Created by kchal on 11/04/2019.
+//
+
+#include <iostream>
+#include <csp/csp.h>
+
+using csp::One2OneChan;
+
+int main(int argc, char **argv) {
+    One2OneChan<int> a;
+
+}
